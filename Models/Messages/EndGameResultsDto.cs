ceced0be20f10f99fe42f91796c97a3501d2bed7
radkam2000 <radkam2000@protonmail.com>
2024-05-12 09:53:46 +0200

    feat: change properties of EndGameResultsDto to correct types

--- Models/Messages/EndGameResultsDto.cs
+++ Models/Messages/EndGameResultsDto.cs
@@ -2,11 +2,10 @@ using Harmonify.Models;
 
 namespace Harmonify.Messages;
 
-//TODO make proper properties for this model (not in scope of task dev-74)
 public class EndGameResultsDto
 {
-  public required Track Track { get; set; }
-  public required RoundResult RoundResult { get; set; }
+  public required List<Track> Tracks { get; set; }
+  public required List<RoundResult> RoundResults { get; set; }
   public required int Score { get; set; }
   public required List<PlayerDto> Players { get; set; }
 }
