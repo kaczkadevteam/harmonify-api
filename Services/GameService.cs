3481a16b4fc58e1687d025e09441d92280a1a0a2
MarcinSkic <marcinskicoficjal@gmail.com>
2024-05-24 11:46:16 +0200

    fix(EndRound): add results to all players before sending response

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -206,6 +206,8 @@ public class GameService(IGameRepository gameRepository, IWebSocketSenderService
 
     game.State = GameState.RoundFinish;
 
+    game.Players.ForEach((player) => AssertPlayerHasAllRoundResults(player, game.CurrentRound));
+
     var playersDto = game
       .Players.Select(
         (player) =>
@@ -227,9 +229,9 @@ public class GameService(IGameRepository gameRepository, IWebSocketSenderService
     await StartNextRound(game);
   }
 
-  private async Task SendPlayerRoundResult(Game game, Player player, List<PlayerDto> playersDto)
+  private static void AssertPlayerHasAllRoundResults(Player player, int currentRound)
   {
-    if (player.RoundResults.Count != game.CurrentRound)
+    while (player.RoundResults.Count < currentRound)
     {
       player.RoundResults.Add(
         new RoundResult
@@ -240,7 +242,10 @@ public class GameService(IGameRepository gameRepository, IWebSocketSenderService
         }
       );
     }
+  }
 
+  private async Task SendPlayerRoundResult(Game game, Player player, List<PlayerDto> playersDto)
+  {
     var response = new MessageWithData<RoundFinishedDto>
     {
       Type = MessageType.NextRound,
